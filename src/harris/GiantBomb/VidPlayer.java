304a49dc61c9a7f6514d71541d731224d29e43d5
poserdonut@f2d6d1aa-d9ff-11de-a65c-977ad3277207 <poserdonut@f2d6d1aa-d9ff-11de-a65c-977ad3277207>
2009-12-02 10:55:05 +0000

    Added a buffering dialog for when the video is loading, might not work to well. Try it out!
    --HG--
    extra : convert_revision : svn%3Af2d6d1aa-d9ff-11de-a65c-977ad3277207/trunk%407

--- src/harris/GiantBomb/VidPlayer.java
+++ src/harris/GiantBomb/VidPlayer.java
@@ -1,8 +1,11 @@
 package harris.GiantBomb;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -11,15 +14,55 @@ import android.widget.VideoView;
  *
  */
 public class VidPlayer extends Activity {
-
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setRequestedOrientation(0); // landscape
-		VideoView vid = new VideoView(this);
+		final VideoView vid = new VideoView(this);
 		setContentView(vid);
 		MediaController controller = new MediaController(this);
 		vid.setMediaController(controller);
 		Bundle bundle = getIntent().getExtras();
 		vid.setVideoURI(Uri.parse(bundle.getString("URL")));
-	}
+		vid.stopPlayback();
+		
+		final ProgressDialog dialog = ProgressDialog.show(VidPlayer.this, "", 
+                "Buffering. Please wait...", true);
+        dialog.show();
+		
+		final Handler handler = new Handler() {
+			@Override
+			public void handleMessage(Message message) {
+				if(message.what == -1) {
+					dialog.dismiss();
+					vid.start();
+				}
+			}
+		};
+
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				
+				try{
+					Boolean buffering = true;
+					int buffer = 0;
+					while(buffering) {
+						if(buffer < vid.getBufferPercentage())
+							buffer = vid.getBufferPercentage();
+						if(buffer > 24)
+							buffering = false;
+					}
+					Message message;
+            		message = handler.obtainMessage(-1);
+            		handler.sendMessage(message);
+				} catch (Throwable t){
+				}
+			}
+		};
+		thread.start();
+		}
 }
+
+
+
