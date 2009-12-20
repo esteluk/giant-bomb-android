bfe3b95390d2db596a8bf6917b81b6a2669aa5ce
andrew.schrauf@f2d6d1aa-d9ff-11de-a65c-977ad3277207 <andrew.schrauf@f2d6d1aa-d9ff-11de-a65c-977ad3277207>
2009-12-20 10:56:21 +0000

    Added related items for GBObjects
    --HG--
    extra : convert_revision : svn%3Af2d6d1aa-d9ff-11de-a65c-977ad3277207/trunk%4040

--- src/harris/GiantBomb/GBObject.java
+++ src/harris/GiantBomb/GBObject.java
@@ -2,6 +2,8 @@ package harris.GiantBomb;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,6 +12,8 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class GBObject implements api {
@@ -23,6 +27,11 @@ public class GBObject implements api {
 	private String id;
 	private String description;
 	private String url;
+	private List<GBObject> related;
+	
+	public GBObject() {
+		setRelated(new ArrayList<GBObject>());
+	}
 
 	public ObjectType getType() {
 		return type;
@@ -64,45 +73,108 @@ public class GBObject implements api {
 		this.url = url;
 	}
 
+	public void setRelated(List<GBObject> related) {
+		this.related = related;
+	}
+
+	public List<GBObject> getRelated() {
+		return related;
+	}
+	
+	public List<GBObject> getRelated(ObjectType type) {
+		List<GBObject> result = new ArrayList<GBObject>();
+		
+		for (GBObject o : related) {
+			if (o.getType() == type) {
+				result.add(o);
+			}
+		}
+		
+		return result;
+	}
+
 	public static GBObject getGBObject(String id, ObjectType type)
 			throws ParserConfigurationException, FactoryConfigurationError,
 			SAXException, IOException {
 		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance()
 				.newDocumentBuilder();
-		URL url = new URL("http://api.giantbomb.com/"
-				+ type.toString().toLowerCase() + "/" + id + "/?api_key="
-				+ API_KEY + "&field_list=id,name,description,site_detail_url&format=xml");
+		URL url = new URL(
+				"http://api.giantbomb.com/"
+						+ type.toString().toLowerCase()
+						+ "/"
+						+ id
+						+ "/?api_key="
+						+ API_KEY
+						+ "&field_list=id,name,description,site_detail_url,games,franchises,characters,concepts,objects,locations,people,companies&format=xml");
 		Document doc = docBuilder.parse(url.openConnection().getInputStream());
 
-		return parseObject((Element) doc.getElementsByTagName("results").item(0), type);
+		return parseObject((Element) doc.getElementsByTagName("results")
+				.item(0), type);
 	}
 
 	public static GBObject parseObject(Element el, ObjectType type) {
 		GBObject item = new GBObject();
-		item.setName(el.getElementsByTagName("name").item(0).getFirstChild()
-				.getNodeValue());
-		item.setId(el.getElementsByTagName("id").item(0).getFirstChild()
-				.getNodeValue());
-
-		try {
-			item.setDescription(el.getElementsByTagName("description").item(0)
-					.getFirstChild().getNodeValue());
-		} catch (Exception e) {
-		}
-		;
 		
+		NodeList nodes = el.getChildNodes();
 		try {
-			item.setUrl(el.getElementsByTagName("site_detail_url").item(0)
-					.getFirstChild().getNodeValue());
-		} catch (Exception e) {
+			for (int i = 0; i <= nodes.getLength(); i++) {
+				String nodeName = nodes.item(i).getNodeName();
+				
+				if (nodeName.equals("name")) {
+					item.setName(nodes.item(i).getFirstChild().getNodeValue());
+				} else if (nodeName.equals("id")) {
+					item.setId(nodes.item(i).getFirstChild().getNodeValue());
+				} else if (nodeName.equals("description")) {
+					item.setDescription(nodes.item(i).getFirstChild().getNodeValue());
+				} else if (nodeName.equals("site_detail_url")) {
+					item.setUrl(nodes.item(i).getFirstChild().getNodeValue());
+				}
+			}
+		} catch (Exception e) {}
+		
+//		item.setName(el.getElementsByTagName("name").item(0).getFirstChild()
+//				.getNodeValue());
+//		item.setId(el.getElementsByTagName("id").item(0).getFirstChild()
+//				.getNodeValue());
+//
+//		try {
+//			item.setDescription(el.getElementsByTagName("description").item(0)
+//					.getFirstChild().getNodeValue());
+//		} catch (Exception e) {
+//		}
+//		;
+//
+//		try {
+//			item.setUrl(el.getElementsByTagName("site_detail_url").item(0)
+//					.getFirstChild().getNodeValue());
+//		} catch (Exception e) {
+//		}
+//		;
+
+		for (ObjectType ot : ObjectType.values()) {
+			try {
+				NodeList nl = el.getElementsByTagName(ot.toString().toLowerCase());
+				for (int i = 0; i < (nl.getLength() < 5 ? nl.getLength() : 5); i++) {
+					Element el2 = (Element)nl.item(i);
+					GBObject ri = new GBObject();
+					ri.setName(el2.getElementsByTagName("name").item(0).getFirstChild()
+							.getNodeValue());
+					ri.setId(el2.getElementsByTagName("id").item(0).getFirstChild()
+							.getNodeValue());
+					ri.setType(ot);
+					if (item.getId() != ri.getId() && item.getType() != ri.getType()) {
+						item.getRelated().add(ri);
+					}
+				}
+			} catch (Exception e) {
+			}
 		}
-		;
 
 		item.setType(type);
 
 		return item;
 	}
-	
+
 	public GBObject clone() {
 		GBObject obj = new GBObject();
 		obj.setId(this.id);
@@ -110,8 +182,8 @@ public class GBObject implements api {
 		obj.setType(this.type);
 		obj.setUrl(this.url);
 		obj.setDescription(this.description);
-		
+		obj.setRelated(this.related);
+
 		return obj;
 	}
-
 }
